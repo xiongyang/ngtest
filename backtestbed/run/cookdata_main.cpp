8354cfb08c70b6665a2521ef319ca3b98114a107
Xiong Yang <snake.xiongyang@qq.com>
2016-04-11 08:04:59 +0800

    compile

--- backtestbed/run/cookdata_main.cpp
+++ backtestbed/run/cookdata_main.cpp
@@ -438,7 +438,7 @@ TestResult HandleRequestNetwrok(TestRequest& request, std::map<std::string, Node
             resultfutures.push_back(std::move(future_result));
         }
 
-        std::this_thread::sleep_for(std::chrono::std::chrono::milliseconds (100));
+        std::this_thread::sleep_for(std::chrono::milliseconds (100));
     }
 
 
